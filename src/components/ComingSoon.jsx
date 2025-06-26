573e8f97cfa5c7d27247658c83d2317e570a780d
Pray45 <prayvijaybhai10@gmail.com>
2025-06-26 13:32:37 +0530

    shape

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,9 +1,11 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { Anton, Poppins } from "next/font/google";
+import "./animate.css"
 
 const anton = Anton({ subsets: ['latin'], weight: "400" });
 const poppins = Poppins({ subsets: ['latin'], weight: "400" });
+const poppinsBold = Poppins({ subsets: ['latin'], weight: "800" });
 
 const ComingSoon = () => {
   const [timeLeft, setTimeLeft] = useState({
@@ -42,17 +44,19 @@ const ComingSoon = () => {
       {/* NAVBAR */}
       <header className="flex flex-wrap justify-center items-center h-auto px-4 py-2 bg-[#EBD6A7] text-[#053125]">
         <div className='flex flex-wrap justify-center items-center gap-3 w-full max-w-7xl'>
-          <p className={`${anton.className} hidden text-xs md:text-xl 2xl:text-3xl border-y-[3px] border-[#93A39A] text-[#E72E0A] py-1`}>
+          <p className={`${anton.className} hidden vs:flex text-xs md:text-xl 2xl:text-3xl border-y-[3px] border-[#93A39A] text-[#E72E0A] py-1`}>
             Insert Brain. Press Start. Hack.
           </p>
-
-          <div className="w-35 md:w-40 2xl:w-60 flex justify-center">
-            <div className={`${anton.className} bg-[#E72E0A] h-10 md:h-12 2xl:h-16 w-full shadow-md text-white text-xl md:text-2xl 2xl:text-4xl flex justify-center items-center`} style={{ clipPath: "polygon(0% 0%, 100% 0%, 90% 50%, 100% 100%, 0% 100%, 10% 50%)" }}>
-              CODE ARCADE
-            </div>
+          <div className='mx-5 relative flex justify-center items-center'>
+            <div class="l-shape rotate-180 absolute -left-5 "></div>
+              <div className="w-35 md:w-40 2xl:w-60">
+                <divrelative className={`${anton.className} bg-[#E72E0A] h-10 md:h-12 2xl:h-16 w-full shadow-md text-white text-xl md:text-2xl 2xl:text-4xl flex justify-center items-center`} style={{ clipPath: "polygon(0% 0%, 100% 0%, 90% 50%, 100% 100%, 0% 100%, 10% 50%)" }}>
+                  CODE ARCADE
+                </divrelative >
+              </div>
+            <div class=" -right-5 l-shape absolute"></div>
           </div>
-
-          <p className={`${anton.className} hidden text-xs md:text-xl 2xl:text-3xl border-y-[3px] border-[#93A39A] text-[#E72E0A] py-1`}>
+          <p className={`${anton.className} hidden vs:flex text-xs md:text-xl 2xl:text-3xl border-y-[3px] border-[#93A39A] text-[#E72E0A] py-1`}>
             Prepare For The Ultimate Battle
           </p>
         </div>
@@ -65,16 +69,13 @@ const ComingSoon = () => {
             COMING SOON
           </h1>
 
-          <p className={`${poppins.className} text-[10px] md:text-sm 2xl:text-2xl text-center mt-4`}>
-            Get ready to enter the digital battlefield where code meets creativity.
-          </p>
-          <p className={`${poppins.className} text-[10px] md:text-sm 2xl:text-2xl text-center`}>
-            The most electrifying hackathon of 2025 is loading...
+          <p className={`${poppins.className} text-white text-xs md:w-150 2xl:w-250 md:text-sm 2xl:text-2xl text-center mt-4`}>
+            Get ready to enter the digital battlefield where code meets creativity.The most electrifying hackathon of 2025 is loading...
           </p>
         </div>
 
         <div className='w-full mt-5 vs:mt-0'>
-          <h1 className={`${anton.className} text-[#E72E0A] text-5xl text-center md:text-5xl 2xl:text-8xl vs:mt-0 mt-10 text-shadow-retro-red`}>
+          <h1 className={`${anton.className} text-[#E72E0A] text-5xl text-center md:text-5xl 2xl:text-8xl vs:mt-0 text-shadow-retro-red`}>
             LAUNCHING IN
           </h1>
 
@@ -86,23 +87,25 @@ const ComingSoon = () => {
               { label: "SECONDS", value: timeLeft.seconds }
             ].map(({ label, value }, i) => (
               <div key={i} className="flex flex-col items-center justify-center mb-10">
-                <div className={`${anton.className} bg-[#EBD6A7] text-black font-bold text-3xl md:text-5xl 2xl:text-7xl vvs:w-21 vvs:h-21 w-18 h-18 md:w-24 md:h-24 2xl:w-32 2xl:h-32 rounded flex items-center justify-center`}>
+                <div className={`${anton.className} bg-[#EBD6A7] text-black font-bold text-5xl md:text-5xl 2xl:text-7xl vvs:w-21 vvs:h-21 w-18 h-18 md:w-24 md:h-24 2xl:w-32 2xl:h-32 rounded flex items-center justify-center shadow-[0_0_10px_#fdf3d1] vs:shadow-[0_0_25px_#fdf3d1]`}>
                   {String(value).padStart(2, '0')}
                 </div>
-                <h1 className={`${anton.className} text-sm md:text-xl 2xl:text-4xl mt-2 md:mt-4`}>{label}</h1>
+                <h1 className={`${anton.className} text-white text-lg md:text-2xl 2xl:text-4xl mt-2 md:mt-4`}>{label}</h1>
               </div>
             ))}
           </section>
         </div>
 
-        <div className='w-full px-2 vs:mb-0 vs:mt-0 mt-10 mb-10 flex justify-center'>
-          <div className='bg-[#EBD6A7] w-full max-w-3xl px-4 py-4 md:py-6 2xl:py-10 text-center'>
-            <p className={`${anton.className} text-xs md:text-lg 2xl:text-3xl text-black`}>
-              Sharpen your skills, assemble your team, and get ready
+        <div className='flex-col 2xl:w-4xl md:w-xl px-2 vs:mb-0 vs:mt-0 flex items-center'>
+          <div className='bg-[#EBD6A7] w-full px-4 py-6 md:py-6 2xl:py-10 text-center shadow-[5px_5px_0_#004656]'>
+            <p className={`${poppinsBold.className} text-[13px] md:px-10 md:text-lg 2xl:text-3xl text-black`}>
+              Sharpen your skills, assemble your team, and get ready to code your way to victory!
             </p>
-            <p className={`${anton.className} text-xs md:text-lg 2xl:text-3xl text-black`}>
-              to code your way to victory!
-            </p>
+          </div>
+          <div className='flex mt-4 max-w-3xl justify-between w-full'>
+            <p className={`${poppinsBold.className} text-[12px] md:text-lg text-[#EE9B01]`} >Epic Challenges</p>
+            <p className={`${poppinsBold.className} text-[12px] md:text-lg text-white `} >Amazing Prizes</p>
+            <p className={`${poppinsBold.className} text-[12px] md:text-lg text-[#EBD6A7]`} >Non Stop Action</p>
           </div>
         </div>
       </main>
